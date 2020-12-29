d62d6ebf68c91fb0f96ec6db7426a25735c8277c
minhitsoft <67570180+minhitsoft@users.noreply.github.com>
2020-12-29 15:55:29 +0700

    add

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -3,7 +3,6 @@ function loading() {
     document.getElementById("loading").style.visibility = "visible";
     setTimeout('loadingVisible()', 2500);
 }
-loading()
 function loadingVisible() {
     document.getElementById("loading").style.visibility = "hidden";
 
